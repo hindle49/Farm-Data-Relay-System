f4d85121127ddde14f296410850ed766048df8b8
Ian Hindle <hindle49@gmail.com>
2024-08-09 17:01:12 +0100

    Not sure

--- examples/6_ESPNOW_Sensor/6_ESPNOW_Sensor.ino
+++ examples/6_ESPNOW_Sensor/6_ESPNOW_Sensor.ino
@@ -10,31 +10,37 @@
 #include <Adafruit_BMP280.h>
 #include <fdrs_node.h>
 
+float data1;
+float data2;
 
-Adafruit_BMP280 bmp;
 
 bool status = false;      
 
 void setup() {
   beginFDRS();
    
-  while (!bmp.begin(0x76)) {
-    Serial.println("BMP not initializing!");
-    delay(1000);  // 1 second
-  }
+
   
 }
 void loop() {
 
-  loopFDRS();
-  
-  loadFDRS(bmp.readTemperature(), TEMP_T);
-  //loadFDRS(bme.readHumidity(), HUMIDITY_T);
-  loadFDRS(bmp.readPressure() / 100.0F, PRESSURE_T);
-  sendFDRS();
+  data1 = readHum();
+  loadFDRS(data1, HUMIDITY_T);
+  data2 = readTemp();
+  loadFDRS(data2, TEMP_T);
+//  DBG(sendFDRS());
+    if(sendFDRS()){
+      DBG("Big Success!");
+    } else {
+      DBG("Nope, not so much.");
+    }
+  sleepFDRS(10);  //Sleep time in seconds
+}
 
- 
-  sleepFDRS(60);  //Sleep time in seconds
-  
+float readTemp() {
+  return 22.069;
+}
 
+float readHum() {
+  return random(0, 100);
 }
